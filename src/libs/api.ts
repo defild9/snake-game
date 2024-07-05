9aab449d03f910c75b88d9be43d15152c876521e
Yevhenii Biletskyi <biletskijzena@protonmail.com>
2024-07-06 02:31:32 +0300

    Created api queries

---
+++ src/libs/api.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const addScore = async (name: string, score: number) => {
+  try {
+    const response = await axios.post(`${process.env.API_URL}/add-scores`, {
+      name,
+      score,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error adding score:", error);
+    throw error;
+  }
+};
+
+export const getLeaderboard = async () => {
+  try {
+    const response = await axios.get(`${process.env.API_URL}/leaderboard`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching leaderboard:", error);
+    throw error;
+  }
+};
